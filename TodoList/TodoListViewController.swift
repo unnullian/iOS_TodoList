133f584fa144275b81781234a8a13946c70b1eef
joonwon lee <cafielo@gmail.com>
2020-03-19 14:36:59 +0900

    7 remove & delete button tap handle

--- TodoList/TodoListViewController.swift
+++ TodoList/TodoListViewController.swift
@@ -181,7 +181,7 @@ class TodoListCell: UICollectionViewCell {
     
     
     var doneButtonTapHandler: ((Bool) -> Void)?
-    var removeButtonTapHandler: (() -> Void)?
+    var deleteButtonTapHandler: (() -> Void)?
     
     override func awakeFromNib() {
         super.awakeFromNib()
@@ -204,18 +204,29 @@ class TodoListCell: UICollectionViewCell {
     
     private func showStrikeThrough(_ show: Bool) {
         if show {
-            strikeThroughWidth.constant
-            = descriptionLabel.bounds.width
+            strikeThroughWidth.constant = descriptionLabel.bounds.width
         } else {
-            strikeThroughWidth.constant
-                = 0
+            strikeThroughWidth.constant = 0
         }
     }
     
     func reset() {
-//        strikeThroughView.isHidden = true
-//        removeButton.isHidden = true
+        descriptionLabel.alpha =  1
+        deleteButton.isHidden = true
         showStrikeThrough(false)
+    }
+    
+    @IBAction func checkButtonTapped(_ sender: Any) {
+        checkButton.isSelected = !checkButton.isSelected
+        let isDone = checkButton.isSelected
+        showStrikeThrough(isDone)
+        descriptionLabel.alpha = isDone ? 0.2 : 1
+        deleteButton.isHidden = !isDone
+        doneButtonTapHandler?(isDone)
+    }
+    
+    @IBAction func deleteButtonTapped(_ sender: Any) {
+        deleteButtonTapHandler?()
     }
 }
 
