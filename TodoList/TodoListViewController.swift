543b9e6bfe43097a77bf622a5de29ed182644dc8
joonwon lee <cafielo@gmail.com>
2020-03-19 11:25:01 +0900

    2 가짜데이터로 일단 셀 채워서 보기

--- TodoList/TodoListViewController.swift
+++ TodoList/TodoListViewController.swift
@@ -9,22 +9,53 @@
 import UIKit
 
 class TodoListViewController: UIViewController {
-
     override func viewDidLoad() {
         super.viewDidLoad()
+    }
+}
 
-        // Do any additional setup after loading the view.
+extension TodoListViewController: UICollectionViewDataSource {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 10
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "TodoListCell", for: indexPath) as? TodoListCell else {
+            return UICollectionViewCell()
+        }
+        
+        return cell
     }
-    */
+    
+    func collectionView(_ collectionView: UICollectionView, viewForSupplementaryElementOfKind kind: String, at indexPath: IndexPath) -> UICollectionReusableView {
+        switch kind {
+        case UICollectionView.elementKindSectionHeader:
+            guard let header = collectionView.dequeueReusableSupplementaryView(ofKind: kind, withReuseIdentifier: "TodoListHeaderView", for: indexPath) as? TodoListHeaderView else {
+                return UICollectionReusableView()
+            }
+            return header
+        default:
+            return UICollectionReusableView()
+        }
+    }
+}
 
+extension TodoListViewController: UICollectionViewDelegateFlowLayout {
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        let width: CGFloat = collectionView.bounds.width
+        let height: CGFloat = 60
+        return CGSize(width: width, height: height)
+    }
+}
+
+class TodoListCell: UICollectionViewCell {
+    override func awakeFromNib() {
+        super.awakeFromNib()
+    }
+}
+
+class TodoListHeaderView: UICollectionReusableView {
+    override func awakeFromNib() {
+        super.awakeFromNib()
+    }
 }
