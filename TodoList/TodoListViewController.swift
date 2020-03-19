85652d898d91a09d1ce19c570610783758f4becd
joonwon lee <cafielo@gmail.com>
2020-03-19 12:30:14 +0900

    4 키보드 처리, BG 탭 처리

--- TodoList/TodoListViewController.swift
+++ TodoList/TodoListViewController.swift
@@ -9,8 +9,32 @@
 import UIKit
 
 class TodoListViewController: UIViewController {
+    
+    @IBOutlet weak var inputViewBottom: NSLayoutConstraint!
+    @IBOutlet weak var inputTextField: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        NotificationCenter.default.addObserver(self, selector: #selector(adjustInputView), name: UIResponder.keyboardWillShowNotification, object: nil)
+        NotificationCenter.default.addObserver(self, selector: #selector(adjustInputView), name: UIResponder.keyboardWillHideNotification, object: nil)
+    }
+    
+    
+    @objc private func adjustInputView(noti: Notification) {
+        guard let userInfo = noti.userInfo else { return }
+        guard let keyboardFrame = (userInfo[UIResponder.keyboardFrameEndUserInfoKey] as? NSValue)?.cgRectValue else { return }
+        if noti.name == UIResponder.keyboardWillShowNotification {
+            let adjustmentHeight = keyboardFrame.height - view.safeAreaInsets.bottom
+            inputViewBottom.constant += adjustmentHeight
+        } else {
+            inputViewBottom.constant = 0
+        }
+        print("---> Keyboard End Frame: \(keyboardFrame)")
+    }
+    
+    @IBAction func tapBG(_ sender: Any) {
+        print("---> tap bg")
+        inputTextField.resignFirstResponder()
     }
 }
 
